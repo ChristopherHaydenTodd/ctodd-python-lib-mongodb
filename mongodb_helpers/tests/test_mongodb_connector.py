b9161b06f83592cc4ec2bc47b1040db281c03cba
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-21 16:14:42 -0500

    Adding stub test files for the package, will be populated as package is filled

---
+++ mongodb_helpers/tests/test_mongodb_connector.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Test File for mongodb_connector.py
+"""
+
+# Python Library Imports
+import os
+import sys
+import pytest
+from unittest import mock
+
+# Import File to Test
+from mongodb_helpers import mongodb_connector
+
+
+###
+# Fixtures
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Mocked Functions
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Test Payload
+###
+
+
+# None at the Moment (Empty Test Suite)
